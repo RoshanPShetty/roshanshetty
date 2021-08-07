43e4145506dc27642745c6b71eabe3f1024e4d75
Roshan Shetty <roshanshetty2000@gmail.com>
2021-08-07 18:17:25 -0400

    Header implemented

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./header.css";
+import Web from "./Web/index";
+import Mobile from "./Mobile/index";
 
 function Header() {
-  return <div>This is header</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="header">
+      <div className="logo">Roshan Shetty</div>
+      <div className="menu">
+        <div className="web-menu">
+          <Web />
+        </div>
+        <div className="mobile-menu">
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <i class="fi-rr-apps menu-icon"></i>
+          </div>
+          {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
