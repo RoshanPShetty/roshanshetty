c63d6656af6a14cb946a97965731fbcd0de7ffe3
Roshan Shetty <roshanshetty2000@gmail.com>
2021-08-08 13:25:01 -0400

    Skills section implemented

---
+++ src/Components/Data/skills.js
@@ -0,0 +1,32 @@
+export const SkillsData = [
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "ReactJS",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "JavaScript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
+      },
+      {
+        name: "TypeScript",
+        icon: <i class="devicon-typescript-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Backend",
+    list: [
+      {
+        name: "Python",
+        icon: <i class="devicon-python-plain colored"></i>,
+      },
+      {
+        name: "NodeJS",
+        icon: <i class="devicon-nodejs-plain colored"></i>,
+      },
+    ],
+  },
+];
