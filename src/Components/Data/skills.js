e7b2fe5b34508c71637a7871fa265f5edf0da571
Roshan Shetty <roshanshetty2000@gmail.com>
2021-08-08 15:46:49 -0400

    New skills added

--- src/Components/Data/skills.js
+++ src/Components/Data/skills.js
@@ -7,12 +7,16 @@ export const SkillsData = [
         icon: <i class="devicon-react-original colored"></i>,
       },
       {
-        name: "JavaScript",
-        icon: <i class="devicon-javascript-plain colored"></i>,
+        name: "HTML",
+        icon: <i class="devicon-html5-plain colored"></i>,
       },
       {
-        name: "TypeScript",
-        icon: <i class="devicon-typescript-plain colored"></i>,
+        name: "CSS",
+        icon: <i class="devicon-css3-plain colored"></i>,
+      },
+      {
+        name: "JavaScript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
       },
     ],
   },
@@ -24,8 +28,29 @@ export const SkillsData = [
         icon: <i class="devicon-python-plain colored"></i>,
       },
       {
+        name: "R",
+        icon: <i class="devicon-r-original colored"></i>,
+      },
+      {
         name: "NodeJS",
         icon: <i class="devicon-nodejs-plain colored"></i>,
+      },
+      {
+        name: "Java",
+        icon: <i class="devicon-java-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Data Management",
+    list: [
+      {
+        name: "PostreSQL",
+        icon: <i class="devicon-postgresql-plain colored"></i>,
+      },
+      {
+        name: "MongoDB",
+        icon: <i class="devicon-mongodb-plain colored"></i>,
       },
     ],
   },
