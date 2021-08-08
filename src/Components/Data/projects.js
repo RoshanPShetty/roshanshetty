88a610300fd7159b26fac006a7c61666be89dba9
Roshan Shetty <roshanshetty2000@gmail.com>
2021-08-08 15:35:26 -0400

    New projects added

--- src/Components/Data/projects.js
+++ src/Components/Data/projects.js
@@ -4,9 +4,37 @@ export const ProjectData = [
     title: "DishSaver",
     about:
       "DishSaver is an interactive web application allowing communication between NGOs (Non-profit organizations) and UMass Dining Commons about provision of leftover food from restarurants, etc which would otherwise be wasted.",
-    tags: ["HTML", "CSS", "NodeJS", "Bootstrap", "Heroku"],
+    tags: [
+      "HTML",
+      "CSS",
+      "NodeJS",
+      "JavaScript",
+      "Bootstrap",
+      "Heroku",
+      "GitHub",
+    ],
     demo: "https://dishsaver.herokuapp.com/",
     github: "https://github.com/RoshanPShetty/DishSaver",
     image: require("../../assets/project-images/dishsaver.jpg").default,
+  },
+  {
+    id: 2,
+    title: "Lennstop",
+    about:
+      "Lensstop is a responsive photography website built showcasing photos that I have taken over the years.",
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap", "GitHub"],
+    demo: "https://roshanpshetty.github.io/Lensstop/",
+    github: "https://github.com/RoshanPShetty/Lensstop",
+    image: require("../../assets/project-images/lensstop.jpg").default,
+  },
+  {
+    id: 3,
+    title: "UMass Design",
+    about:
+      "UMassDesign is a Flutter application that is built for the UMass Design Club. It was built in order to increase interactivity for the club and give a platform for everyone to post their work.",
+    tags: ["Flutter", "GitHub"],
+    // demo: "https://roshanpshetty.github.io/Lensstop/",
+    github: "https://github.com/RoshanPShetty/UMassDesign",
+    image: require("../../assets/project-images/umassdesign.jpg").default,
   },
 ];
