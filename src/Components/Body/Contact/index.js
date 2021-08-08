c1601b0e68f0728ebb6e06839070bd0b9f5836a4
Roshan Shetty <roshanshetty2000@gmail.com>
2021-08-08 15:17:05 -0400

    contact section implemented

--- src/Components/Body/Contact/index.js
+++ src/Components/Body/Contact/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "./contact.css";
+import Separator from "../../Common/Separator/index";
+import SocialContact from "../../Common/Social-Contact/index";
 
 function Contact() {
-  return <div>This is contact</div>;
+  return (
+    <div className="contact">
+      <Separator />
+      <label className="section-title">Contact</label>
+      <div className="contact-container">
+        <div className="contact-left">
+          <p>Want to get in touch?</p>
+          <SocialContact />
+        </div>
+        <div className="download">
+          <a
+            download
+            href={
+              require("../../../assets/Shetty_Roshan_Praveen_Resume.pdf")
+                .default
+            }
+          >
+            <i class="fi-rr-download"></i>Download Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
