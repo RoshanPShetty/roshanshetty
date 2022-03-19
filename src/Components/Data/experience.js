03b4c68140a5e92a720e172376e1ca6228e5e28c
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2022-03-18 23:39:34 -0400

    Experience updated

--- src/Components/Data/experience.js
+++ src/Components/Data/experience.js
@@ -10,7 +10,7 @@ export const ExperienceData = [
       "https://pbs.twimg.com/profile_images/1348913543983165440/MDrjijV6_400x400.png",
   },
   {
-    company: "College of Social & Behavioral Sciences - UMass Amherst",
+    company: "UMass Amherst College of Social and Behavioral Sciences",
     title: "Fullstack Developer",
     dateStart: "Jun. 2021",
     dateEnd: "Aug. 2021",
@@ -25,7 +25,17 @@ export const ExperienceData = [
     dateStart: "Jan. 2021",
     dateEnd: "Jan. 2022",
     description:
-      "Offering residential experience supporting students’ learning, personal growth, and academic achievement. Fostering inclusive communities, creating social and educational opportunities, and providing safe and well-maintained residences.",
+      "Offered residential experience supporting students’ learning, personal growth, and academic achievement. Fostering inclusive communities, creating social and educational opportunities, and providing safe and well-maintained residences.",
+    companyLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/UMass_Amherst_Athletics_logo.svg/2505px-UMass_Amherst_Athletics_logo.svg.png",
+  },
+  {
+    company: "UMass Amherst School of Public Health & Health Sciences",
+    title: "IT Assistantt",
+    dateStart: "Jan. 2022",
+    dateEnd: "Present.",
+    description:
+      "Supporting and maintaining over 400 in-house computer systems and desktops for the School of Public Health through ServiceNow and Tenable.io to detect vulnerabilities. Using KACE Systems for imaging computers and configuring drivers.",
     companyLogo:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/UMass_Amherst_Athletics_logo.svg/2505px-UMass_Amherst_Athletics_logo.svg.png",
   },
